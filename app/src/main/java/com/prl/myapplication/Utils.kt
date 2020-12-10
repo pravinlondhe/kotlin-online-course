5661f8cc4441ada2f7926a3376dafbd628599d27
Pravin Londhe <pravinlondhe15@gmail.com>
2020-12-10 07:14:45 +0530

    extension function and default value argument function

--- app/src/main/java/com/prl/myapplication/Utils.kt
+++ app/src/main/java/com/prl/myapplication/Utils.kt
@@ -4,5 +4,5 @@ import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 fun RecyclerView.ViewHolder.showToast(msg: String, duration: Int = Toast.LENGTH_LONG) {
-    Toast.makeText(this.itemView.context, msg, duration).show()
+    Toast.makeText(itemView.context, msg, duration).show()
 }

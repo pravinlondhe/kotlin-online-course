bd391a29085b1b45eb7a3e850d46517c51e88104
Pravin Londhe <pravinlondhe15@gmail.com>
2020-12-10 07:28:38 +0530

    extension function - added layoutRes annotation

--- app/src/main/java/com/prl/myapplication/Utils.kt
+++ app/src/main/java/com/prl/myapplication/Utils.kt
@@ -4,10 +4,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView
 
 fun RecyclerView.ViewHolder.showToast(msg: String, duration: Int = Toast.LENGTH_LONG) {
     Toast.makeText(itemView.context, msg, duration).show()
 }
 
-fun ViewGroup.inflate(layout: Int): View = LayoutInflater.from(context).inflate(layout, this, false)
+fun ViewGroup.inflate(@LayoutRes layout: Int): View = LayoutInflater.from(context).inflate(layout, this, false)

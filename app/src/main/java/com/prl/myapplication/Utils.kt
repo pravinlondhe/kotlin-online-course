7ea41a052b3ebdd232430408c434a50815702d6e
Pravin Londhe <pravinlondhe15@gmail.com>
2020-12-14 07:55:36 +0530

    lesson-6 lambda with receiver

--- app/src/main/java/com/prl/myapplication/Utils.kt
+++ app/src/main/java/com/prl/myapplication/Utils.kt
@@ -10,12 +10,47 @@ import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView
 
-fun RecyclerView.ViewHolder.showToast(msg: String, duration: Int = Toast.LENGTH_LONG) {
+fun RecyclerView.ViewHolder.showToast(msg: String?, duration: Int = Toast.LENGTH_LONG) {
     Toast.makeText(itemView.context, msg, duration).show()
 }
 
-fun ViewGroup.inflate(@LayoutRes layout: Int): View = LayoutInflater.from(context).inflate(layout, this, false)
+fun ViewGroup.inflate(@LayoutRes layout: Int): View =
+    LayoutInflater.from(context).inflate(layout, this, false)
 
-inline fun <reified T: Activity> Context.startActivity(){
+inline fun <reified T : Activity> Context.startActivity() {
     startActivity(Intent(this, T::class.java))
+}
+
+// Lambada with receiver
+
+//1. let -> Takes it returns last statement
+inline fun <T, U> T.myLet(body: (T) -> U): U {
+    return body(this)
+}
+
+//2. run -> Takes this returns last statement
+inline fun <T, U> T.myRun(body: T.() -> U): U {
+    return body()
+}
+
+//2.1 run -> takes nothing returns last statement
+inline fun <T> myRun2(body: () -> T): T {
+    return body()
+}
+
+//3. also -> Takes it returns this object
+inline fun <T> T.myAlso(body: (T) -> Unit): T {
+    body(this)
+    return this
+}
+
+//4. apply -> Takes this returns this object
+inline fun <T> T.myApply(body: T.() -> Unit): T {
+    body()
+    return this
+}
+
+//5. with -> Takes this returns last statement
+inline fun <T, U> myWith(receiver: T, body: T.() -> U): U {
+    return receiver.body()
 }
